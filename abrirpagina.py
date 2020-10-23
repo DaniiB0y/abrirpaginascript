30c2f1722d14a58a3df0b3bc3fb2b55ba4338950
DannyBoy <neo3nt3r@gmail.com>
2020-10-23 17:06:30 -0300

    Agora funciona com qualquer link usando web scrap

--- abrirpagina.py
+++ abrirpagina.py
@@ -1,18 +1,23 @@
 from string import *
 import webbrowser as web
+from bs4 import BeautifulSoup as bs
+import requests
 
-# Mostrar url usando o navegador padrão. Se o novo for 0, a url é aberto na mesma janela do navegador,
-# se possível. Se é novo 1, uma nova janela do navegador é aberto, se possível.
 
 fala = str(input('Digite seu comando: '))
-#Pegando a frase 
 if(fala.count("abrir")):
     fala = fala.replace("abrir", "")
-    fala = fala.replace(" ", "")
-    #tirando abrir pra ficar só o nome do site
-    #tirando o espaço para ficar sem espaço, 22/10/2020 não funcionou com .split()
-    print(f" Abrindo {fala}")
-    #Informando ao usuario que está sendo aberto
-    web.open(f'https://www.{fala}.com', new=0, autoraise=True)
-else:
-    print("Eror 404")
+    fala = fala.replace(" ", "+")
+    #Tratamento de string
+    e = "&"
+    #O python não reconhece o & então vamos armazenar em uma variavel
+    page = requests.get(f"https://www.google.com/search?q={fala}{e}oq={fala}{e}aqs=chrome..69i57j35i39j35i39i457j69i60l2j69i65j69i60l2.652j0j7&sourceid=chrome&ie=UTF-8")
+    soup = bs(page.text, 'html.parser')
+    # Parseamento e pegando conteudo da pagina
+    links = soup.find_all("a")
+    # Pegando todos os links e selecionando um
+    link = links[17]
+    link = link['href']
+    link = link.replace("/url?q=", "")
+    # Tratamento de string
+    web.open(f'{link}', new=0, autoraise=True)
